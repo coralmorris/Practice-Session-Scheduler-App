10361f6291b03d4b263699309491e06573dc94f0
CoralMorris <coral.morris33@gmail.com>
2018-11-12 11:20:13 -0500

    Add project files.

---
+++ PracticeSessionSchedulerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeSessionSchedulerApp.Startup))]
+namespace PracticeSessionSchedulerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
